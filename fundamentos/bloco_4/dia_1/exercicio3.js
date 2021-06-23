a1048e8ad582332b6ee0dcf92872273ca6ea225e
Lucas-GSS <lucasgabrieldesouza9@gmail.com>
2021-06-23 20:22:24 -0300

    Exercicio 3 resolvido

---
+++ fundamentos/bloco_4/dia_1/exercicio3.js
@@ -0,0 +1,10 @@
+let number1 = 1;
+let number2 = 3;
+let number3 = 1;
+if (number1 > number2 && number1 > number3) {
+    console.log("O maior número é", number1)
+} else if (number2 > number1 && number2 > number3) {
+    console.log("O maior número é", number2)
+} else {
+    console.log("O maior número é", number3)
+};
