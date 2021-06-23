6c78e66939f02a3f1f37de9729e2013a4dc4c0eb
Lucas-GSS <lucasgabrieldesouza9@gmail.com>
2021-06-23 20:28:34 -0300

    Exercicio 4 resolvido

---
+++ fundamentos/bloco_4/dia_1/exercicio4.js
@@ -0,0 +1,8 @@
+let ramdomNumber = -5;
+if (ramdomNumber > 0) {
+    console.log("O número é positivo")
+} else if (ramdomNumber < 0) {
+    console.log("O número é negativo")
+} else {
+    console.log("O número é 0")
+};
