833a1ab90cebe8b65db351ef930294975a40a1a8
Lucas-GSS <lucasgabrieldesouza9@gmail.com>
2021-07-23 15:38:47 -0300

    Parte 2 : exercicio 4.2 resolvido

--- fundamentos/bloco-7-introducao-a-javascript-es6-teste-unitarios/dia_1-let-const-arrow-functions-template_literal/last_exercise.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-teste-unitarios/dia_1-let-const-arrow-functions-template_literal/last_exercise.js
@@ -1,2 +1,7 @@
-const changeString = (phrase, word) => phrase.substring(0).replace('x',word);
-console.log(changeString("Tryber x aqui!","Lucas"));
+const changeString = (phrase, word) => console.log(phrase.substring(0).replace('x',word));
+changeString("Tryber x aqui!","Lucas");
+
+const skills = ['Persistencia', 'Esforço', 'Dedicação', 'Ambição', 'Foco'];
+
+const addSkills = (string, array) => console.log(`Minhas principais habilidades são:${array.sort()}`);
+addSkills(changeString("Tryber x aqui!","Lucas"), skills);
