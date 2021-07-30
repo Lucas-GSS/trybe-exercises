8f9895cbe0a820ba95aad78d170d6c4532be3133
Lucas-GSS <lucasgabrieldesouza9@gmail.com>
2021-07-30 08:40:28 -0300

    Exercicios parte 1 resolvidos

--- fundamentos/bloco-7-introducao-a-javascript-es6-teste-unitarios/dia_2-objects/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-teste-unitarios/dia_2-objects/script.js
@@ -36,14 +36,20 @@ const order = {
 
 const customerInfo = (order) => {
   // Adicione abaixo as informações necessárias.
-
+  console.log(`Olá ${order.order.delivery.deliveryPerson}, entrega para:
+  ${order.name}, Telefone: ${order.phoneNumber}, R: ${order.address.street},
+  Nº: ${order.address.number}, AP: ${order.address.apartment}`)
 }
 
 customerInfo(order);
 
 const orderModifier = (order) => {
   // Adicione abaixo as informações necessárias.
-
+  order['name'] = 'Luiz Silva';
+  order.payment['total'] = 50;
+  const pizza = Object.keys(order.order.pizza);
+  console.log(`Olá ${order.name}, o total do seu pedido de ${pizza[0]}, ${pizza[1]} e 
+  ${order.order.drinks.coke.type} é R$ ${order.payment.total}`)
 }
 
 orderModifier(order);
