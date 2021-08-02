ea0bf1d7888d5fee93ca811687ea3a00ab279a2b
Lucas-GSS <lucasgabrieldesouza9@gmail.com>
2021-08-02 09:24:36 -0300

    Exercicio 3 - parte 2 resolvido

--- fundamentos/bloco-7-introducao-a-javascript-es6-teste-unitarios/dia_3-testes-unitarios-javascript/pt2-exercise3.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-teste-unitarios/dia_3-testes-unitarios-javascript/pt2-exercise3.js
@@ -1,5 +1,12 @@
 const assert = require('assert');
 // escreva a função sumAllNumbers aqui
+const sumAllNumbers = (array) => {
+  let output = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    output = output + array[index];
+  }
+  return output;
+}
 
 const numbers = [9, 23, 10, 3, 8];
 const expected = 53;
