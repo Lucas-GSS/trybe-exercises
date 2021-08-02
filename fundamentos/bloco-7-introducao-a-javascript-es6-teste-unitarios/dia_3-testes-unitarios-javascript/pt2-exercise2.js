41c69015b7877e9c38d8b36a9e21394959e18b0f
Lucas-GSS <lucasgabrieldesouza9@gmail.com>
2021-08-02 09:18:29 -0300

    Exercicio 2 - parte 2 resolvido

--- fundamentos/bloco-7-introducao-a-javascript-es6-teste-unitarios/dia_3-testes-unitarios-javascript/pt2-exercise2.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-teste-unitarios/dia_3-testes-unitarios-javascript/pt2-exercise2.js
@@ -1,6 +1,12 @@
 const assert = require('assert');
 // escreva a função wordLengths aqui
-
+const wordLengths = (array) => {
+  const output = [];
+  for (let index = 0; index < array.length; index += 1) {
+    output.push(array[index].length);
+  }
+  return output;
+}
 const words = ['sun', 'potato', 'roundabout', 'pizza'];
 const expected = [3, 6, 10, 5];
 
