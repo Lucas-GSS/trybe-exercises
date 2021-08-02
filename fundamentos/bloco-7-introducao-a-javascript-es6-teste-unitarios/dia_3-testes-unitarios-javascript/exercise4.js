efd49e57f622b6e4283192e5cfd5cd666d5a6bf9
Lucas-GSS <lucasgabrieldesouza9@gmail.com>
2021-08-02 08:37:34 -0300

    Exercicio 4 resolvido

--- fundamentos/bloco-7-introducao-a-javascript-es6-teste-unitarios/dia_3-testes-unitarios-javascript/exercise4.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-teste-unitarios/dia_3-testes-unitarios-javascript/exercise4.js
@@ -8,4 +8,14 @@ function myFizzBuzz(num) {
   return num;
 }
 
-// implemente seus testes aqui
+assert.strictEqual(typeof myFizzBuzz, 'function');
+
+assert.strictEqual(myFizzBuzz(15), 'fizzbuzz');
+
+assert.strictEqual(myFizzBuzz(9), 'fizz');
+
+assert.strictEqual(myFizzBuzz(20), 'buzz');
+
+assert.strictEqual(myFizzBuzz(8), 8);
+
+assert.strictEqual(myFizzBuzz(a), false, 'O parâmetro tem que ser um número');
