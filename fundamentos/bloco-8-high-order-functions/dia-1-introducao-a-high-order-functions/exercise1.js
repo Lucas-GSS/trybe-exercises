b4a410aacb2ffed78e90c6a47c7e1a0c5f91989a
Lucas-GSS <lucasgabrieldesouza9@gmail.com>
2021-08-03 23:44:54 -0300

    Exercicio 1 resolvido

--- fundamentos/bloco-8-high-order-functions/dia-1-introducao-a-high-order-functions/exercise1.js
+++ fundamentos/bloco-8-high-order-functions/dia-1-introducao-a-high-order-functions/exercise1.js
@@ -1,8 +1,11 @@
+const employeesData = fullName => ({nomeCompleto: fullName, email: fullName.toLowerCase().split(' ').join('_') + '@trybe.com',});
+
 const newEmployees = () => {
   const employees = {
-    id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    id1: employeesData('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: employeesData('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: employeesData('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
   }
   return employees;
 };
+console.log(newEmployees());
