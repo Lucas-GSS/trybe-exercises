e825f2d6bbe6933fd8481222389e6b53b8bd895e
Lucas-GSS <lucasgabrieldesouza9@gmail.com>
2021-08-12 22:07:06 -0300

    Exercicio 5 resolvido

--- fundamentos/bloco-8-high-order-functions/dia-5-hof-spread-operator-parâmetro-rest-destructuring-e-mais/exercise5.js
+++ fundamentos/bloco-8-high-order-functions/dia-5-hof-spread-operator-parâmetro-rest-destructuring-e-mais/exercise5.js
@@ -5,6 +5,7 @@ const myList = [1, 2, 3];
 // Escreva a função swap , que dado um array de 3 elementos, retorna um novo array com o primeiro e terceiro elementos trocados. Detalhe: você precisa fazer essa função gastando 1 linha só:
 
 // escreva swap abaixo
+const swap = ([first, second, third ]) => [third,second,first];
 
 const swappedList = swap(myList);
 
