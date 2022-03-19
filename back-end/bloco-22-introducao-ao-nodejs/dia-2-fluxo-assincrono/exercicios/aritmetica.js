fc30cb0db43010c94422674d5eebcda71be2bac7
Lucas Gabriel <lucasgabrieldesouza9@gmail.com>
2022-03-19 09:31:13 -0300

    Exercicio 2: refatora para usar async/await

--- back-end/bloco-22-introducao-ao-nodejs/dia-2-fluxo-assincrono/exercicios/aritmetica.js
+++ back-end/bloco-22-introducao-ao-nodejs/dia-2-fluxo-assincrono/exercicios/aritmetica.js
@@ -1,16 +1,28 @@
-function aritimetica(num1, num2, num3) {
-  const promise = new Promise((resolve, reject) => {
-    if(typeof(num1) !== 'number' || typeof(num2) !== 'number' || typeof(num3) !== 'number')
-      reject(new Error('Informe apenas números'))
-    const resultado = (num1 + num2) * num3;
-    resolve(resultado > 50 ? resultado : 'Valor muito baixo')
-  });
-  return promise
-};
+// function aritimetica(num1, num2, num3) {
+//   const promise = new Promise((resolve, reject) => {
+//     if(typeof(num1) !== 'number' || typeof(num2) !== 'number' || typeof(num3) !== 'number')
+//       reject(new Error('Informe apenas números'))
+//     const resultado = (num1 + num2) * num3;
+//     resolve(resultado > 50 ? resultado : 'Valor muito baixo')
+//   });
+//   return promise
+// };
 
 const randomNum1 = Math.floor(Math.random() * 50 + 1);
 const randomNum2 = Math.floor(Math.random() * 50 + 1);
 const randomNum3 = Math.floor(Math.random() * 50 + 1);
-aritimetica(randomNum1, randomNum2, randomNum3)
-.then((valor) => console.log(valor))
-.catch((err) => console.error(err));
+
+async function aritimetica(num1, num2, num3) {
+  const promise = await new Promise((resolve, reject) => {
+    if(typeof(num1) !== 'number' || typeof(num2) !== 'number' || typeof(num3) !== 'number')
+      reject(new Error('Informe apenas números'))
+    const resultado = (num1 + num2) * num3;
+    resolve(resultado > 50 ? resultado : 'Valor muito baixo')
+  })
+  try{
+    console.log(promise)
+  } catch(err) {
+    console.error(err)
+  }
+}
+aritimetica(randomNum1, randomNum2, randomNum3);
