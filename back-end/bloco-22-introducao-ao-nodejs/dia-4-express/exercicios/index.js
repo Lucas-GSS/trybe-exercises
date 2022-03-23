d38f9f6bd183373b6cceb43912b819ceba894164
Lucas Gabriel <lucasgabrieldesouza9@gmail.com>
2022-03-23 20:11:52 -0300

    Implementa a rota /hello

--- back-end/bloco-22-introducao-ao-nodejs/dia-4-express/exercicios/index.js
+++ back-end/bloco-22-introducao-ao-nodejs/dia-4-express/exercicios/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 app.get('/ping', (req, res) => {
   return res.status(200).json({ message: 'pong' });
 });
 
-const port = 3001;
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  return res.status(200).json({ message: `Hello, ${name}` });
+})
 
+
+
+const port = 3001;
 app.listen(port, console.log(`Running on locallhost:${port}`));
