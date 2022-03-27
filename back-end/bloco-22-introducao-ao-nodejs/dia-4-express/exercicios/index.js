70809e6ff244480bd4c5942cd6b87aa0de39d856
Lucas Gabriel <lucasgabrieldesouza9@gmail.com>
2022-03-27 10:22:07 -0300

    Implementa a rota /greetings

--- back-end/bloco-22-introducao-ao-nodejs/dia-4-express/exercicios/index.js
+++ back-end/bloco-22-introducao-ao-nodejs/dia-4-express/exercicios/index.js
@@ -11,6 +11,13 @@ app.get('/ping', (req, res) => {
 app.post('/hello', (req, res) => {
   const { name } = req.body;
   return res.status(200).json({ message: `Hello, ${name}` });
+});
+
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
+
+  if( Number(age) <= 17) return res.status(401).json({ message: 'Unauthorized' });
+  return res.status(200).json({ message: `Hello, ${name}` });
 })
 
 
