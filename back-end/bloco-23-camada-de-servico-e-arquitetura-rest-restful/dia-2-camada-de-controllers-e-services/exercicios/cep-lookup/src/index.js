0de176566fb8b71848b35e098850970f9d957aba
Lucas Gabriel <lucasgabrieldesouza9@gmail.com>
2022-04-07 22:59:49 -0300

    Adiciona as rotas /cep e /ping no app

--- back-end/bloco-23-camada-de-servico-e-arquitetura-rest-restful/dia-2-camada-de-controllers-e-services/exercicios/cep-lookup/src/index.js
+++ back-end/bloco-23-camada-de-servico-e-arquitetura-rest-restful/dia-2-camada-de-controllers-e-services/exercicios/cep-lookup/src/index.js
@@ -7,8 +7,12 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const PORT = process.env.PORT;
 
+const cepRoute = require('./routes/cepRoute');
+
 app.use(express.json());
 
 app.use('/ping', pingRoute);
+
+app.use('/cep', cepRoute)
 
 app.listen(PORT, console.log(`Listening on port ${PORT}`));
