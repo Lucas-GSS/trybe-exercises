45a868b257b980239a5abbbe300055b248b91bc2
Lucas Gabriel <lucasgabrieldesouza9@gmail.com>
2022-04-09 15:03:19 -0300

    padroniza respostas para o formato json e com status code correspondente ao sucesso/falha da requisição

--- back-end/bloco-23-camada-de-servico-e-arquitetura-rest-restful/dia-3-rest-e-restful/exercicio/controllers/productController.js
+++ back-end/bloco-23-camada-de-servico-e-arquitetura-rest-restful/dia-3-rest-e-restful/exercicio/controllers/productController.js
@@ -5,36 +5,35 @@ const router = express.Router();
 
 router.get('/', async (req, res, next) => {
   const products = await ProductModel.getAll();
-
-  res.send(products);
+  if(!products) return res.status(404).json({ message: 'nenhum produto encontrado' })
+  return res.status(200).json({products});
 });
 
 router.get('/:id', async (req, res, next) => {
   const product = await ProductModel.getById(req.params.id);
-
-  res.send(product);
+  if (!product) return res.status(404).json({ message: 'Produto não encontrado' }) 
+  res.status(200).json({ product });
 });
 
 router.post('/', async (req, res) => {
   const { name, brand } = req.body;
-
+  if (!name || !brand) return res.staus(400).json({ message: 'Nome e marca do produto são obrigatórios' })
   const newProduct = await ProductModel.add(name, brand);
-
-  res.send(newProduct);
+  if (!newProduct) return res.status(500).json({ message: 'Erro ao adicionar o produto' })
+  res.status(201).json({message: `Produto criado com sucesso: ${newProduct}`});
 });
 
 router.delete('/:id', async (req, res) => {
   const products = await ProductModel.exclude(req.params.id);
-
-  res.send(products);
+  if (!products) res.status(404).json({ message: 'Não foi possível excluir pois não foi encontrado' })
+  res.status(200).json({message: `Produto excluído: ${products}` });
 });
 
 router.put('/:id', async (req, res) => {
   const { name, brand } = req.body;
-
   const products = await ProductModel.update(req.params.id, name, brand);
-
-  res.send(products);
+  if (!products) return res.status(404).json({ message: 'Não foi possível atualizar pois não foi encontrado' })
+  res.status(200).json({message: `Atualizado: ${products}`});
 });
 
 module.exports = router;
