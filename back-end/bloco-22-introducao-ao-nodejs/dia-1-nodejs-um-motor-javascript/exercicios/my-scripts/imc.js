a146bbff4720d70d7dff4fa7df784ea2a3cdda27
Lucas Gabriel <lucasgabrieldesouza9@gmail.com>
2022-06-08 10:38:55 -0300

    exercicios 3 e 4: torna o script interativo e adicina tabela do IMC

--- back-end/bloco-22-introducao-ao-nodejs/dia-1-nodejs-um-motor-javascript/exercicios/my-scripts/imc.js
+++ back-end/bloco-22-introducao-ao-nodejs/dia-1-nodejs-um-motor-javascript/exercicios/my-scripts/imc.js
@@ -1,8 +1,22 @@
+const readline = require('readline-sync');
+
 console.log('Calculo do IMC');
+
+const peso = readline.questionFloat('Qual seu peso(kg)? ');
+const altura = readline.questionFloat('Qual sua altura(metro)? ');
 
 const imcCalc = (peso, altura) => peso / (altura ** 2);
 
-console.log(`Uma pessoa com o peso de 85kg e 1.85m de altura tem o IMC equivalente a:
-${imcCalc(85, 1.85).toFixed(2)}.
-Segundo a tabela o IMC essa pessoa está com peso normal
+console.log(`
+  Seu IMC é: ${imcCalc(peso, altura).toFixed(2)}!
+  Confira abaixo os Indices:
+`);
+console.log(`
+IMC	                Situação
+Abaixo de 18,5	    Abaixo do peso (magreza)
+Entre 18,5 e 24,9	  Peso normal
+Entre 25,0 e 29,9	  Acima do peso (sobrepeso)
+Entre 30,0 e 34,9	  Obesidade grau I
+Entre 35,0 e 39,9	  Obesidade grau II
+40,0 e acima	      Obesidade graus III e IV
 `);
