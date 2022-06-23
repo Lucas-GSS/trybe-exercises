772f70dcae1cc0defdc7c77d09e92dfd6f507e4c
Lucas Gabriel <lucasgabrieldesouza9@gmail.com>
2022-06-23 15:03:31 -0300

    exercicios 4 a 8: criar uma api de consulta e adição de personagens dos simpsons

--- back-end/bloco-22-introducao-ao-nodejs/dia-4-express-http-com-node/exercicio/index.js
+++ back-end/bloco-22-introducao-ao-nodejs/dia-4-express-http-com-node/exercicio/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+const fs = require('fs/promises');
 
 app.use(express.json());
 
@@ -15,6 +16,30 @@ app.post('/greetings', (req, res) => {
   const { name, age } = req.body;
   if (age < 18) return res.status(401).json({ message: 'Unauthorized' });
   return res.status(200).json({ message: `Seu nome é ${name} e você tem ${age} anos de idade` });
+});
+
+app.get('/simpsons', async (_req, res) => {
+  const simpsons = await fs.readFile('./simpsons.json', 'utf-8');
+  if (!simpsons) return res.status(404).json({ message: 'No characters found' });
+  return res.status(200).json({ message: JSON.parse(simpsons) });
+});
+
+app.get('/simpsons/:id', async (req, res) => {
+  const { id } = req.params;
+  const simpsons = await fs.readFile('./simpsons.json', 'utf-8');
+  const selectedSimpson = JSON.parse(simpsons).find((simpson) => simpson.id === id);
+  if (!selectedSimpson) return res.status(404).json({ message: 'Character not found' });
+  return res.status(200).json({ message: selectedSimpson });
+});
+
+app.post('/simpsons', async (req, res) => {
+  const { id, name } = req.body;
+  const simpsons = JSON.parse(await fs.readFile('./simpsons.json', 'utf-8'));
+  const alreadyExists = simpsons.find((simpson) => simpson.id === id);
+  if (alreadyExists) return res.status(409).json({ message: 'Character already exists' });
+  simpsons.push({ id, name });
+  await fs.writeFile('./simpsons.json', JSON.stringify(simpsons));
+  return res.status(204).end()
 });
 
 app.listen(3001, console.log('rodando na 3001'));
