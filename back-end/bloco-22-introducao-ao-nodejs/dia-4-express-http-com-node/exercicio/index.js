8ac7d1c469fcbacf2a6197fc6c43796805bededa
Lucas Gabriel <lucasgabrieldesouza9@gmail.com>
2022-06-23 14:30:49 -0300

    exercicio 3: criar rota post /greetings

--- back-end/bloco-22-introducao-ao-nodejs/dia-4-express-http-com-node/exercicio/index.js
+++ back-end/bloco-22-introducao-ao-nodejs/dia-4-express-http-com-node/exercicio/index.js
@@ -11,4 +11,10 @@ app.post('/hello', (req, res) => {
   return res.status(200).json({ message: `Hello, ${name}` });
 })
 
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
+  if (age < 18) return res.status(401).json({ message: 'Unauthorized' });
+  return res.status(200).json({ message: `Seu nome é ${name} e você tem ${age} anos de idade` });
+});
+
 app.listen(3001, console.log('rodando na 3001'));
