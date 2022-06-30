09dabadf4f4c79ec08bc71d1c2c2ee99fc5d8630
Lucas Gabriel <lucasgabrieldesouza9@gmail.com>
2022-06-30 20:33:23 -0300

    adiciona a rota /books à aplicação

--- back-end/bloco-23-camada-de-servico-e-arquitetura-rest-restful/dia-1-camada-de-model/fixacao/model_example/index.js
+++ back-end/bloco-23-camada-de-servico-e-arquitetura-rest-restful/dia-1-camada-de-model/fixacao/model_example/index.js
@@ -3,9 +3,12 @@ const express = require('express');
 require('dotenv').config();
 
 const authorRouter = require('./routes/authorRoute');
+const bookRouter = require('./routes/booksRoute');
 
 const app = express();
 
 app.use('/authors', authorRouter);
+
+app.use('/books', bookRouter)
 
 app.listen(process.env.PORT, console.log(`Rodando na ${process.env.PORT}`));
