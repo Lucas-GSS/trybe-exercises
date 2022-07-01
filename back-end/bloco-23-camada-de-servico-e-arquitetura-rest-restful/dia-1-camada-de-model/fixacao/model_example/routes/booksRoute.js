ccbb53543635f2d6945b52ddf13231c9e46d0197
Lucas Gabriel <lucasgabrieldesouza9@gmail.com>
2022-07-01 13:03:55 -0300

    rota get /books/:id

--- back-end/bloco-23-camada-de-servico-e-arquitetura-rest-restful/dia-1-camada-de-model/fixacao/model_example/routes/booksRoute.js
+++ back-end/bloco-23-camada-de-servico-e-arquitetura-rest-restful/dia-1-camada-de-model/fixacao/model_example/routes/booksRoute.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const { getAll, getById } = require('../model/booksModel');
+const { getAll, getByAuthorId, getById } = require('../model/booksModel');
 
 const router = express.Router();
 
 router.get('/:id', async(req, res) => {
   const { id } = req.params;
-  const books = await getById(id);
+  const book = await getById(id);
+  return res.status(200).json(book);
+})
+
+router.get('/search/', async(req, res) => {
+  const { authorId } = req.query;
+  const books = await getByAuthorId(authorId);
   return res.status(200).json(books);
 });
 
