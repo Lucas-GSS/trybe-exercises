d1645a785ec3ed0dfb6f6f7d1ca053931091d84a
Lucas Gabriel <lucasgabrieldesouza9@gmail.com>
2022-07-01 13:03:11 -0300

    rota get /authors/:id

--- back-end/bloco-23-camada-de-servico-e-arquitetura-rest-restful/dia-1-camada-de-model/fixacao/model_example/routes/authorRoute.js
+++ back-end/bloco-23-camada-de-servico-e-arquitetura-rest-restful/dia-1-camada-de-model/fixacao/model_example/routes/authorRoute.js
@@ -1,8 +1,14 @@
 const express = require('express');
 
-const getAll = require('../model/authorModel');
+const { getAll, getById } = require('../model/authorModel');
 
 const router = express.Router();
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const author = await getById(id);
+  return res.status(200).json(author);
+});
 
 router.get('/', async (_req, res) => {
   const authors = await getAll();
