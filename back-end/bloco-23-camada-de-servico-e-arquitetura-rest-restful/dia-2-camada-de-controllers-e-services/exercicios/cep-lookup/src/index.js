4eb0a518dd12dce7750b129a299e20a10a60d177
Lucas Gabriel <lucasgabrieldesouza9@gmail.com>
2022-07-08 08:32:27 -0300

    feat: middleware de erro e rota get /cep/:cep

--- back-end/bloco-23-camada-de-servico-e-arquitetura-rest-restful/dia-2-camada-de-controllers-e-services/exercicios/cep-lookup/src/index.js
+++ back-end/bloco-23-camada-de-servico-e-arquitetura-rest-restful/dia-2-camada-de-controllers-e-services/exercicios/cep-lookup/src/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const pingRouter = require('./routes/pingRoute');
-
 require('express-async-errors');
+
+const cepRouter = require('./routes/cepRouter');
+const pingRouter = require('./routes/pingRoute');
 
 require('dotenv').config();
 
@@ -9,6 +10,21 @@ const app = express();
 
 const { PORT } = process.env;
 
+app.use(express.json());
+
+app.use('/cep', cepRouter);
+
 app.use('/ping', pingRouter);
+
+const errorMap = {
+  ValidationError: 400,
+  NotFound: 404,
+};
+
+app.use(({ name, message }, _req, res, _next) => {
+  const code = errorMap[name];
+  if (!code) return res.status(500).json({ message: 'Internal error server' });
+  return res.status(code).json({ message });
+});
 
 app.listen(PORT, console.log(`rodando na ${PORT}`));
