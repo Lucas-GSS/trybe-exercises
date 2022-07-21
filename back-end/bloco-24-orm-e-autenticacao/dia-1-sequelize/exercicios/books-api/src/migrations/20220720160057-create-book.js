c7409ae60048130faff494fb1e67479a47d8cc0e
Lucas Gabriel <lucasgabrieldesouza9@gmail.com>
2022-07-21 14:02:14 -0300

    feat(sequelize): Book migration

---
+++ back-end/bloco-24-orm-e-autenticacao/dia-1-sequelize/exercicios/books-api/src/migrations/20220720160057-create-book.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+  /**
+   * 
+   * @param {import('sequelize').QueryInterface} queryInterface 
+   * @param {import('sequelize').Sequelize} Sequelize 
+   */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      author: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      pageQuantity: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        field: 'page_quantity'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'created_at'
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'updated_at'
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Books');
+  }
+};
