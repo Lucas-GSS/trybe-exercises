91903fcc05d7c58c79b6d2b3b3ff3ddd627dd615
Lucas Gabriel <lucasgabrieldesouza9@gmail.com>
2022-07-21 14:04:11 -0300

    feat: raiz da aplicação express

--- back-end/bloco-24-orm-e-autenticacao/dia-1-sequelize/exercicios/books-api/src/index.js
+++ back-end/bloco-24-orm-e-autenticacao/dia-1-sequelize/exercicios/books-api/src/index.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const errorHandler = require('./middleware/errorHandler');
+
+require('express-async-errors');
 
 const app = express();
 
+const booksRouter = require('./routes/booksRouter')
+
 app.use(express.json());
 
+app.use('/books', booksRouter);
+
+app.use(errorHandler)
+
 app.listen(3000, () => console.log('Rodando na 3000'));
+
+// para não ter que importar o dotenv, usar o comando "-r dotenv/config" no meio do  depois do --watch src script start ou dev
