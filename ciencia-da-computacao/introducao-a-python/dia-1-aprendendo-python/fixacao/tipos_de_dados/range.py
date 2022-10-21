77bcc03a38d74a197ce53d10f0961ced2ce7e355
Lucas Gabriel <lucasgabrieldesouza9@gmail.com>
2022-10-21 18:09:56 -0300

    sintaxe, exemplos e anotações sobre range

---
+++ ciencia-da-computacao/introducao-a-python/dia-1-aprendendo-python/fixacao/tipos_de_dados/range.py
@@ -0,0 +1,7 @@
+# estrutura capaz de gerar valores numéricos dado um início, fim e passo
+# o valor de final não é incluso no resultado retornado, portanto para retornar de 1 a 10 range(1, 11);
+
+numeros = list(range(1, 11))
+print(numeros)
+outros_numeros = list(range(0, 21, 2))  # 0 a 20 de 2 em 2
+print(outros_numeros)
